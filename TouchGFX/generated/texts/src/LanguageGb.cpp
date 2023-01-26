564bf5c03d2d389e556ece51380f8e4fc096c0f7
Max V. Belyachkov <maxspb69@gmail.com>
2023-01-26 11:26:05 +0300

    TouchGFX 4.21.0 migrate

--- TouchGFX/generated/texts/src/LanguageGb.cpp
+++ TouchGFX/generated/texts/src/LanguageGb.cpp
@@ -10,16 +10,16 @@ KEEP extern const uint32_t indicesGb[] TEXT_LOCATION_FLASH_ATTRIBUTE;
 // Remap all strings
 TEXT_LOCATION_FLASH_PRAGMA
 KEEP extern const uint32_t indicesGb[] TEXT_LOCATION_FLASH_ATTRIBUTE = {
-    128, // T___SINGLEUSE_GL49: "Back"
-    117, // T___SINGLEUSE_BFL2: "Menu_5"
-    110, // T___SINGLEUSE_KLH9: "Menu_4"
-    103, // T___SINGLEUSE_HGTA: "Menu_3"
-    96,  // T___SINGLEUSE_B1L5: "Menu_2"
-    73,  // T___SINGLEUSE_I7DK: "Menu_1p"
+    127, // T___SINGLEUSE_GL49: "Back"
+    116, // T___SINGLEUSE_BFL2: "Menu_5"
+    109, // T___SINGLEUSE_KLH9: "Menu_4"
+    102, // T___SINGLEUSE_HGTA: "Menu_3"
+    95,  // T___SINGLEUSE_B1L5: "Menu_2"
+    88,  // T___SINGLEUSE_I7DK: "Menu_1"
     63,  // T___SINGLEUSE_KWM7: "Main Menu"
-    81,  // T___SINGLEUSE_ZF8W: "More..."
-    89,  // T___SINGLEUSE_JJU1: "Graph "
+    73,  // T___SINGLEUSE_ZF8W: "More..."
+    81,  // T___SINGLEUSE_JJU1: "Graph "
     52,  // T___SINGLEUSE_UC29: "Set values"
-    124, // T___SINGLEUSE_SZ5P: "<> %"
+    123, // T___SINGLEUSE_SZ5P: "<> %"
     0    // T___SINGLEUSE_KIL9: "Dynamic Graph\n sample in OLED\n 4bit 256x64 \nDisplay"
 };
